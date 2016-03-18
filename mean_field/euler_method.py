cd07e3d7c0152f4f92ba5291e58ec7af480c2bbd
yzerlaut <yann.zerlaut@gmail.com>
2016-03-18 17:09:37 +0100

    up home

--- mean_field/euler_method.py
+++ mean_field/euler_method.py
@@ -9,13 +9,14 @@ def func(t):
     return 2.*np.exp(-(t-1)**2/.1)
 
 def run_mean_field(NRN1, NRN2, NTWK, array_func,\
-                   T=5e-3, dt=1e-4, tstop=2, extended_output=False):
+                   T=5e-3, dt=1e-4, tstop=2, extended_output=False,\
+                   ext_drive_change=0.):
 
     # find external drive
     M = get_connectivity_and_synapses_matrix(NTWK, SI_units=True)
     ext_drive = M[0,0]['ext_drive']
     
-    X0 = find_fixed_point_first_order(NRN1, NRN2, NTWK, exc_aff=ext_drive,\
+    X0 = find_fixed_point_first_order(NRN1, NRN2, NTWK, exc_aff=ext_drive+ext_drive_change,\
                                       verbose=False)
 
     TF1, TF2 = load_transfer_functions(NRN1, NRN2, NTWK)
@@ -24,13 +25,13 @@ def run_mean_field(NRN1, NRN2, NTWK, array_func,\
     
     def dX_dt_scalar(X, t=0):
         return build_up_differential_operator_first_order(TF1, TF2, T=T)(X,\
-                               exc_aff=ext_drive, pure_exc_aff=array_func(t))
+                               exc_aff=ext_drive+ext_drive_change, pure_exc_aff=array_func(t))
     fe, fi = odeint(dX_dt_scalar, X0, t).T         # we don't need infodict here
 
     if extended_output:
         params = get_neuron_params(NRN2, SI_units=True)
         reformat_syn_parameters(params, M)
-        muV, sV, muGn, TvN = get_fluct_regime_vars(fe+ext_drive,\
+        muV, sV, muGn, TvN = get_fluct_regime_vars(fe+ext_drive+ext_drive_change,\
                                                    fi,\
                                                    *pseq_params(params))
         return t, fe, fi, muV, sV, muGn, TvN
