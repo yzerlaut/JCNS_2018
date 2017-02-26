835c26fc1fea0dfc7c68b6c7caebf459b952ec06
yzerlaut <yann.zerlaut@gmail.com>
2017-02-26 14:26:21 +0100

    adapting tf to run on cluster

--- transfer_functions/run_both_tf.sh
+++ transfer_functions/run_both_tf.sh
@@ -5,6 +5,7 @@
 # python theoretical_tools.py -f data/RS-cell_CONFIG1.npy --With_Square
 
 # for cluster run
+source ~/.profile
 srun tf_simulation.py FS-cell CONFIG1 -s --SEED 4 --tstop 10
 sget data/FS-cell_CONFIG1.npy
 python theoretical_tools.py -f data/FS-cell_CONFIG1.npy --With_Square
