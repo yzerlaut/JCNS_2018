7e7c29b77689aabffd49d2881ea7e1dd8a347fcb
yzerlaut <yann.zerlaut@gmail.com>
2017-04-17 16:59:34 +0200

    adjusting simulation time wrt freq

--- dynamical_accuracy/multiple_freq.py
+++ dynamical_accuracy/multiple_freq.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-N = 10
+N = 20
 freqs = np.logspace(-1, np.log(350)/np.log(10), N)
 seeds = np.arange(3)
 amp = 5.
@@ -9,7 +9,7 @@ t0 = 500.
 
 if __name__=='__main__':
     for freq in freqs:
-        ts = max([2*t0, t0+1e3*5/freqs])
+        ts = max([2*t0, t0+1e3*5/freq])
         baseline_cmd = 'python oscillating_input.py -S --amp '+str(amp)+' --tstop '+str(ts)
         for seed in seeds:
             filename = 'data/varying_freq_'+str(freq)+'_seed_'+str(seed)+'.npy'
