72f896cf7c26cc777c8c358019ce50972162a03c
yzerlaut <yann.zerlaut@gmail.com>
2017-04-17 13:06:11 +0200

    including seed variations in oscillating input

--- dynamical_accuracy/multiple_freq.py
+++ dynamical_accuracy/multiple_freq.py
@@ -2,14 +2,15 @@ import os
 import numpy as np
 
 N = 2
-freqs = np.logspace(0, np.log(500)/np.log(10), N)
-seeds = np.arange(len(freqs))
+freqs = np.logspace(-1, np.log(350)/np.log(10), N)
+seeds = np.arange(4)
 tstop = 2000.
 amp = 5.
 t0 = 500.
 
 if __name__=='__main__':
     baseline_cmd = 'python oscillating_input.py -S --amp '+str(amp)+' --tstop '+str(tstop)
-    for freq, seed in zip(freqs, seeds):
-        filename = 'data/varying_freq_'+str(freq)+'.npy'
-        os.system(baseline_cmd+' --freq '+str(freq)+' --SEED '+str(seed)+' --file '+filename)
+    for freq in freqs:
+        for seed in seeds:
+            filename = 'data/varying_freq_'+str(freq)+'_seed_'+str(seed)+'.npy'
+            os.system(baseline_cmd+' --freq '+str(freq)+' --SEED '+str(seed)+' --file '+filename)
