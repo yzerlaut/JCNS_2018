f84ab5d63d48f14b31bab16126f406e44a1b8a4e
yzerlaut <yann.zerlaut@gmail.com>
2017-05-18 15:52:11 +0200

    found settings of data plots

--- experimental_data/plot.py
+++ experimental_data/plot.py
@@ -15,11 +15,11 @@ f = loadmat('matx2_br131023_NL_dw_1020_23.mat')
 data = 1e3*f['matNL'][0]['stim1'][0]
 time = f['matNL'][0]['time'][0].flatten()+100
 space = f['matNL'][0]['space'][0].flatten()
-Nsmooth = 3
+Nsmooth = 2
 smoothing = np.ones((Nsmooth, Nsmooth))/Nsmooth**2
 smooth_data = convolve2d(data, smoothing, mode='same')
 
-cond = (time>-10) & (time<300)
+cond = (time>-10) & (time<250)
 c = ax.contourf(time[cond], space, smooth_data[:,cond], cmap=cm.viridis)
 plt.colorbar(c, label='VSD signal ($\perthousand$)', ticks=-.3+.3*np.arange(5))
 
@@ -27,8 +27,8 @@ ax.plot([0,0], [0,2], '-', color='gray', lw=4)
 ax.annotate('2mm', (0,2), rotation=90, fontsize=14)
 
 def find_latencies_over_space_simple(t, X, signal,\
-                                     signal_criteria=0.3,\
-                                     amp_criteria=0.2):
+                                     signal_criteria=0.4,\
+                                     amp_criteria=.6):
     signal2 = signal.T
     # i_discard = int(discard/(t[1]-t[0]))
     # t = t[i_discard:]
