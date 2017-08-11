bfed23a6540225a7b231ae6967424c42c4654cff
yzerlaut <yann.zerlaut@gmail.com>
2017-08-11 16:37:58 +0200

    ==================== \n if commit works you forgot to commit !!! \n ==========================

--- ring_model/parameter_scan.py
+++ ring_model/parameter_scan.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pylab as plt
 import itertools
+# everything stored within a zip file
+import zipfile
 
 def create_grid_scan_bash_script(args):
 
@@ -10,7 +12,7 @@ def create_grid_scan_bash_script(args):
             ' --conduction_velocity_mm_s '+str(vc)+\
             ' --exc_connect_extent '+str(ecr)+\
             ' --inh_connect_extent '+str(ecr*rIE)+\
-            ' --Tau2 '+str(t2)+' -f '+fn+' --no_plot --X_extent 30 --X_discretization 30 \n'
+            ' --Tau2 '+str(t2)+' -f '+fn+' --no_plot --X_extent 30 --X_discretization 30 & \n'
 
     VC = np.linspace(args.vc[0], args.vc[1], args.N)
     ECR = np.linspace(args.Econn_radius[0], args.Econn_radius[1], args.N)
@@ -27,12 +29,53 @@ def create_grid_scan_bash_script(args):
     f.close()
     np.save('data/scan_data.npy', [VC, ECR, RIE, TAU2, np.array(FILENAMES)])
 
+
+def zip_data(args):
+    zf = zipfile.ZipFile(args.zip_filename, mode='w')
+    # writing the parameters
+    zf.write('data/scan_data.npy')
+    VC, ECR, RIE, TAU2, FILENAMES = np.load('data/scan_data.npy')
+    for fn in FILENAMES:
+        zf.write(fn)
+    zf.close()
+
+def unzip_data(args):
+    zf = zipfile.ZipFile(args.zip_filename, mode='r')
+    # writing the parameters
+    zf.write('data/scan_data.npy')
+    VC, ECR, RIE, TAU2, FILENAMES = np.load('data/scan_data.npy')
+    data = zf.read(filename.replace('data/scan_data.npy'))
+    with open('data/scan_data.npy', 'wb') as f: f.write(data)
+    for fn in FILENAMES:
+        zf.write(fn)
+    zf.close()
+    
+    
+    # if filename is None:
+    #     filename=str(Model['zip_filename'])
+    # zf = zipfile.ZipFile(filename, mode='r')
+    
+    # Model = dict(np.load(filename.replace('.zip', '_Model.npz')).items())
+    
+    # F_aff, seeds = Model['F_AffExc_array'], Model['SEEDS']
+    
+    # DATA = []
+    # for i, j in product(range(len(F_aff)), range(len(seeds))):
+        
+    #     fn = Model['FILENAMES'][i,j]
+    #     data = zf.read(fn)
+    #     with open(fn, 'wb') as f: f.write(data)
+    #     with open(fn, 'rb') as f: data = load_dict_from_hdf5(fn)
+    #     data['faff'], data['seed'] = F_aff[i], seeds[j]
+    #     DATA.append(data)
+        
+    # return Model, F_aff, seeds, DATA
+
 def analyze_scan(args):
     
     VC, ECR, RIE, TAU2, FILENAMES = np.load('data/scan_data.npy')
     print(FILENAMES)
 
-    
     
 if __name__=='__main__':
     import argparse
@@ -49,6 +92,7 @@ if __name__=='__main__':
     parser.add_argument("--Tau1", nargs=2, type=float, default=[5., 100.])
     parser.add_argument("--Tau2", nargs=2, type=float, default=[50., 300.])
     parser.add_argument("--N", type=int, default=2)
+    parser.add_argument("--zip_filename", '-f', type=str, default='data/data.zip')
     parser.add_argument("-a", "--analyze", help="analyze", action="store_true")
     
     args = parser.parse_args()
