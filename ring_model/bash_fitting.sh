8d223a6ae6341e28690c37a03dd08f1f0a88ea64
yzerlaut <yann.zerlaut@gmail.com>
2017-08-14 19:09:55 +0200

    up

--- ring_model/bash_fitting.sh
+++ ring_model/bash_fitting.sh
@@ -1,13 +1,13 @@
-python fitting_model_params.py --N 1000 -df 0 &
-python fitting_model_params.py --N 1000 -df 1 &
-python fitting_model_params.py --N 1000 -df 2 &
-python fitting_model_params.py --N 1000 -df 3 &
-python fitting_model_params.py --N 1000 -df 4 &
-python fitting_model_params.py --N 1000 -df 5 &
-python fitting_model_params.py --N 1000 -df 6 &
-python fitting_model_params.py --N 1000 -df 7 &
-python fitting_model_params.py --N 1000 -df 8 &
-python fitting_model_params.py --N 1000 -df 9 &
-python fitting_model_params.py --N 1000 -df 10 &
-python fitting_model_params.py --N 1000 -df 11 &
-python fitting_model_params.py --N 1000 -df 12 &
+python fitting_model_params.py --N 1000 -df 0 --fitting &
+python fitting_model_params.py --N 1000 -df 1 --fitting &
+python fitting_model_params.py --N 1000 -df 2 --fitting &
+python fitting_model_params.py --N 1000 -df 3 --fitting &
+python fitting_model_params.py --N 1000 -df 4 --fitting &
+python fitting_model_params.py --N 1000 -df 5 --fitting &
+python fitting_model_params.py --N 1000 -df 6 --fitting &
+python fitting_model_params.py --N 1000 -df 7 --fitting &
+python fitting_model_params.py --N 1000 -df 8 --fitting &
+python fitting_model_params.py --N 1000 -df 9 --fitting &
+python fitting_model_params.py --N 1000 -df 10 --fitting &
+python fitting_model_params.py --N 1000 -df 11 --fitting &
+python fitting_model_params.py --N 1000 -df 12 --fitting &
