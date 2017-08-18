64b3536b5a0db61ce724adebbd7abe5bb40e696b
yzerlaut <yann.zerlaut@gmail.com>
2017-08-18 13:30:44 +0200

    up

--- ring_model/bash_fitting.sh
+++ ring_model/bash_fitting.sh
@@ -1,20 +1,20 @@
-# python fitting_model_params.py --N 10 -df 0 --fitting &
-# python fitting_model_params.py --N 10 -df 1 --fitting &
-# python fitting_model_params.py --N 10 -df 2 --fitting &
-# python fitting_model_params.py --N 10 -df 3 --fitting &
-# python fitting_model_params.py --N 10 -df 4 --fitting &
-# python fitting_model_params.py --N 10 -df 5 --fitting &
-# python fitting_model_params.py --N 10 -df 6 --fitting &
-# python fitting_model_params.py --N 10 -df 7 --fitting &
-# python fitting_model_params.py --N 10 -df 8 --fitting &
-# python fitting_model_params.py --N 10 -df 9 --fitting &
-# python fitting_model_params.py --N 10 -df 10 --fitting &
-# python fitting_model_params.py --N 10 -df 11 --fitting &
-# python fitting_model_params.py --N 10 -df 12 --fitting &
+python fitting_model_params.py --N 10 -df 0 --fitting &
+python fitting_model_params.py --N 10 -df 1 --fitting &
+python fitting_model_params.py --N 10 -df 2 --fitting &
+python fitting_model_params.py --N 10 -df 3 --fitting &
+python fitting_model_params.py --N 10 -df 4 --fitting &
+python fitting_model_params.py --N 10 -df 5 --fitting &
+python fitting_model_params.py --N 10 -df 6 --fitting &
+python fitting_model_params.py --N 10 -df 7 --fitting &
+python fitting_model_params.py --N 10 -df 8 --fitting &
+python fitting_model_params.py --N 10 -df 9 --fitting &
+python fitting_model_params.py --N 10 -df 10 --fitting &
+python fitting_model_params.py --N 10 -df 11 --fitting &
+python fitting_model_params.py --N 10 -df 12 --fitting &
 
 # ########################################################
-for i in {0..13}
-do
-    echo sget '../ring_model/data/fitting_data_'$i'.npy'
-done
-echo ''
+# for i in {0..13}
+# do
+#     echo sget '../ring_model/data/fitting_data_'$i'.npy'
+# done
+# echo ''
